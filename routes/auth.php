508b91437d5e4f431a4fb74704b909d539172810
josephkipkemoi <jkemboe@gmail.com>
2022-06-21 10:38:28 -0400

    CSRF

--- routes/auth.php
+++ routes/auth.php
@@ -9,7 +9,6 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
-                ->middleware(['guest','web','cors'])               
                 ->name('register');
 
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])
