26ad679d82fccd6291fea4733a0987c6dc98fc1a
josephkipkemoi <jkemboe@gmail.com>
2022-06-24 15:20:55 -0400

    remove csrf

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'api',
+        'guard' => 'web',
         'passwords' => 'users',
     ],
 
@@ -36,7 +36,7 @@ return [
     */
 
     'guards' => [
-        'api' => [
+        'web' => [
             'driver' => 'session',
             'provider' => 'users',
         ],
