2a00610ee6e6391687cbe9959c96686ef81e5a5c
Joseph Kipkemoi <jkemboe@gmail.com>
2022-06-28 18:48:33 +0300

    Update auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -33,5 +33,4 @@ Route::post('/email/verification-notification', [EmailVerificationNotificationCo
                 ->name('verification.send');
 
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->middleware('auth')
                 ->name('logout');
