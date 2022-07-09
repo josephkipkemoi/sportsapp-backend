dbc8f30b0e2b2ff2c8ac265619b1470538f0d788
josephkipkemoi <jkemboe@gmail.com>
2022-07-09 13:33:01 -0400

    Custom Fixture

---
+++ database/migrations/2022_07_09_172633_update_custom_fixtures_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('custom_fixtures', function (Blueprint $table) {
+            $table->integer('home_odds')->nullable();
+            $table->integer('draw_odds')->nullable();
+            $table->integer('away_odds')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
