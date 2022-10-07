b571803a4b38747f7d1727b16548d4fb295a8be3
josephkipkemoi <jkemboe@gmail.com>
2022-10-08 00:47:54 +0300

    Customer Support Backend Implementation

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => 'ap2',
-                'useTLS' => false,
+                'useTLS' => true,
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
